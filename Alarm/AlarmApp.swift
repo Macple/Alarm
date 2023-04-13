9bca01c624a4bf1e8f76046cbcd402ee76cbf79f
Maciej Plewko <maciej.plewko@gmail.com>
2023-04-13 18:07:33 +0200

    Initial iOS project

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Maciej Plewko on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
